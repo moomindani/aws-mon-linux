6817278c11fee2afbcc3ae4d950102c3ee56915d
root <root@ip-172-31-18-116.ap-northeast-1.compute.internal>
2014-04-08 06:49:16 +0000

    Implements memory-units option and disk-space-units option

--- aws-mon.sh
+++ aws-mon.sh
@@ -13,12 +13,6 @@
 # express or implied. See the License for the specific language governing 
 # permissions and limitations under the License.
 
-# TODO memory-units option
-# TODO disk-space-units option
-# TODO aggregated option
-# TODO auto-scaling option
-
-# Requirement: yum install sysstat
 
 ########################################
 # Initial Settings
@@ -31,6 +25,14 @@ instanceid=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 azone=`wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone`
 region=${azone/%?/}
 export EC2_REGION=$region
+
+
+########################################
+# Constants
+########################################
+KILO=1024
+MEGA=1048576
+GIGA=1073741824
 
 
 ########################################
@@ -59,6 +61,7 @@ usage()
     echo -e "\t--cpu-id\tReports cpu utilization (idle) in percentages."
     echo -e "\t--cpu-wa\tReports cpu utilization (wait) in percentages."
     echo -e "\t--cpu-st\tReports cpu utilization (steal) in percentages."
+    echo -e "\t--memory-units UNITS\tSpecifies units in which to report memory usage. If not specified, memory is reported in megabytes. UNITS may be one of the following: bytes, kilobytes, megabytes, gigabytes."
     echo -e "\t--mem-used-incl-cache-buff\tCount memory that is cached and in buffers as used."
     echo -e "\t--mem-util\tReports memory utilization in percentages."
     echo -e "\t--mem-used\tReports memory used in megabytes."
@@ -67,6 +70,7 @@ usage()
     echo -e "\t--swap-used\tReports allocated swap space in megabytes."
     echo -e "\t--swap-avail\tReports available swap space in megabytes."
     echo -e "\t--disk-path PATH\tSelects the disk by the path on which to report."
+    echo -e "\t--disk-space-units UNITS\tSpecifies units in which to report disk space usage. If not specified, disk space is reported in gigabytes. UNITS may be one of the following: bytes, kilobytes, megabytes, gigabytes."
     echo -e "\t--disk-space-util\tReports disk space utilization in percentages."
     echo -e "\t--disk-space-used\tReports allocated disk space in gigabytes."
     echo -e "\t--disk-space-avail\tReports available disk space in gigabytes."
@@ -78,7 +82,7 @@ usage()
 # Options
 ########################################
 SHORT_OPTS="h"
-LONG_OPTS="help,version,verify,verbose,debug,from-cron,aws-credential-file:,aws-access-key-id:,aws-secret-key:,load-ave1,load-ave5,load-ave15,interrupt,context-switch,cpu-us,cpu-sy,cpu-id,cpu-wa,cpu-st,mem-used-incl-cache-buff,mem-util,mem-used,mem-avail,swap-util,swap-used,swap-avail,disk-path:,disk-space-util,disk-space-used,disk-space-avail,all-items" 
+LONG_OPTS="help,version,verify,verbose,debug,from-cron,aws-credential-file:,aws-access-key-id:,aws-secret-key:,load-ave1,load-ave5,load-ave15,interrupt,context-switch,cpu-us,cpu-sy,cpu-id,cpu-wa,cpu-st,memory-units:,mem-used-incl-cache-buff,mem-util,mem-used,mem-avail,swap-util,swap-used,swap-avail,disk-path:,disk-space-units:,disk-space-util,disk-space-used,disk-space-avail,all-items" 
 
 ARGS=$(getopt -s bash --options $SHORT_OPTS --longoptions $LONG_OPTS --name $SCRIPT_NAME -- "$@" ) 
 
@@ -99,6 +103,8 @@ CPU_SY=0
 CPU_ID=0
 CPU_WA=0
 CPU_ST=0
+MEM_UNITS="megabytes"
+MEM_UNIT_DIV=1
 MEM_USED_INCL_CACHE_BUFF=0
 MEM_UTIL=0
 MEM_USED=0
@@ -107,6 +113,8 @@ SWAP_UTIL=0
 SWAP_USED=0
 SWAP_AVAIL=0
 DISK_PATH=""
+DISK_SPACE_UNITS="gigabytes"
+DISK_SPACE_UNIT_DIV=1
 DISK_SPACE_UTIL=0
 DISK_SPACE_USED=0
 DISK_SPACE_AVAIL=0
@@ -180,6 +188,10 @@ while true; do
             CPU_ST=1
             ;;
         # Memory
+        --memory-units)
+            shift
+            MEM_UNITS=$1
+            ;;
         --mem-used-incl-cache-buff)
             MEM_USED_INCL_CACHE_BUFF=1
             ;;
@@ -205,7 +217,11 @@ while true; do
         --disk-path) 
             shift 
             DISK_PATH=$1
-            ;; 
+            ;;
+        --disk-space-units)
+            shift
+            DISK_SPACE_UNITS=$1
+            ;;
         --disk-space-util)
             DISK_SPACE_UTIL=1
             ;;
@@ -255,7 +271,6 @@ done
 vmstat_output=`/usr/bin/vmstat`
 meminfo_output=`/bin/cat /proc/meminfo`
 uptime_output=`/usr/bin/uptime`
-mpstat_output=`/usr/bin/mpstat`
 df_output=`/bin/df -k -l -P $DISK_PATH`
 
 
@@ -267,6 +282,43 @@ function getMemInfo()
     echo "$meminfo_output" | grep ^$1: | sed -e 's/'$1':\s*\([0-9]*\).*$/\1/'
 }
 
+
+########################################
+# Units Decision
+########################################
+# Memory
+if [ $MEM_UNITS = "bytes" ]; then
+    MEM_UNITS="Bytes"
+    MEM_UNIT_DIV=1
+elif [ $MEM_UNITS = "kilobytes" ]; then
+    MEM_UNITS="Kilobytes"
+    MEM_UNIT_DIV=$KILO
+elif [ $MEM_UNITS = "megabytes" ]; then
+    MEM_UNITS="Megabytes"
+    MEM_UNIT_DIV=$MEGA
+elif [ $MEM_UNITS = "gigabytes" ]; then
+    MEM_UNITS="Gigabytes"
+    MEM_UNIT_DIV=$GIGA
+else
+    echo "Unsupported memory units '$MEM_UNITS'. Use bytes, kilobytes, megabytes, or gigabytes."
+fi
+
+# Disk
+if [ $DISK_SPACE_UNITS = "bytes" ]; then
+    DISK_SPACE_UNITS="Bytes"
+    DISK_SPACE_UNIT_DIV=1
+elif [ $DISK_SPACE_UNITS = "kilobytes" ]; then
+    DISK_SPACE_UNITS="Kilobytes"
+    DISK_SPACE_UNIT_DIV=$KILO
+elif [ $DISK_SPACE_UNITS = "megabytes" ]; then
+    DISK_SPACE_UNITS="Megabytes"
+    DISK_SPACE_UNIT_DIV=$MEGA
+elif [ $DISK_SPACE_UNITS = "gigabytes" ]; then
+    DISK_SPACE_UNITS="Gigabytes"
+    DISK_SPACE_UNIT_DIV=$GIGA
+else
+    echo "Unsupported disk space units '$DISK_SPACE_UNITS'. Use bytes, kilobytes, megabytes, or gigabytes."
+fi
 
 ########################################
 # Main
@@ -291,8 +343,6 @@ if [ $DEBUG -eq 1 ]; then
     echo "$uptime_output"
     echo "-----vmstat-----"
     echo "$vmstat_output"
-    echo "-----mpstat-----"
-    echo "$mpstat_output"
     echo "-----/proc/meminfo-----"
     echo "$meminfo_output"
     echo "-----df-----"
@@ -405,16 +455,22 @@ fi
 
 # Memory
 mem_total=`getMemInfo "MemTotal"`
+mem_total=`expr $mem_total \* $KILO`
 mem_free=`getMemInfo "MemFree"`
+mem_free=`expr $mem_free \* $KILO`
 mem_cached=`getMemInfo "Cached"`
+mem_cached=`expr $mem_cached \* $KILO`
 mem_buffers=`getMemInfo "Buffers"`
+mem_buffers=`expr $mem_buffers \* $KILO`
 mem_avail=$mem_free
 if [ $MEM_USED_INCL_CACHE_BUFF -eq 1 ]; then
     mem_avail=`expr $mem_avail + $mem_cached + $mem_buffers`
 fi
 mem_used=`expr $mem_total - $mem_avail`
 swap_total=`getMemInfo "SwapTotal"`
+swap_total=`expr $swap_total \* $KILO`
 swap_free=`getMemInfo "SwapFree"`
+swap_free=`expr $swap_free \* $KILO`
 swap_avail=$swap_free
 swap_used=`expr $swap_total - $swap_free`
 
@@ -438,20 +494,22 @@ if [ $MEM_UTIL -eq 1 -a $mem_total -gt 0 ]; then
 fi
 
 if [ $MEM_USED -eq 1 ]; then
+    mem_used=`expr $mem_used / $MEM_UNIT_DIV`
     if [ $VERBOSE -eq 1 ]; then
         echo "mem_used:$mem_used"
     fi
     if [ $VERIFY -eq 0 ]; then
-        /opt/aws/bin/mon-put-data --metric-name "MemoryUsed" --value "$mem_used" --unit "Kilobytes" $CLOUDWATCH_OPTS
+        /opt/aws/bin/mon-put-data --metric-name "MemoryUsed" --value "$mem_used" --unit "$MEM_UNITS" $CLOUDWATCH_OPTS
     fi
 fi
 
 if [ $MEM_AVAIL -eq 1 ]; then
+    mem_avail=`expr $mem_avail / $MEM_UNIT_DIV`
     if [ $VERBOSE -eq 1 ]; then
         echo "mem_avail:$mem_avail"
     fi
     if [ $VERIFY -eq 0 ]; then        
-        /opt/aws/bin/mon-put-data --metric-name "MemoryAvailable" --value "$mem_avail" --unit "Kilobytes" $CLOUDWATCH_OPTS
+        /opt/aws/bin/mon-put-data --metric-name "MemoryAvailable" --value "$mem_avail" --unit "$MEM_UNITS" $CLOUDWATCH_OPTS
     fi
 fi
 
@@ -466,27 +524,32 @@ if [ $SWAP_UTIL -eq 1 -a $swap_total -gt 0 ]; then
 fi
 
 if [ $SWAP_USED -eq 1 ]; then
+    swap_used=`expr $swap_used / $MEM_UNIT_DIV`
     if [ $VERBOSE -eq 1 ]; then
         echo "swap_used:$swap_used"
     fi
     if [ $VERIFY -eq 0 ]; then
-        /opt/aws/bin/mon-put-data --metric-name "SwapUsed" --value "$swap_used" --unit "Kilobytes" $CLOUDWATCH_OPTS
+        /opt/aws/bin/mon-put-data --metric-name "SwapUsed" --value "$swap_used" --unit "$MEM_UNITS" $CLOUDWATCH_OPTS
     fi
 fi
 
 if [ $SWAP_AVAIL -eq 1 ]; then
+    swap_avail=`expr $swap_avail / $MEM_UNIT_DIV`
     if [ $VERBOSE -eq 1 ]; then
         echo "swap_avail:$swap_avail"
     fi
     if [ $VERIFY -eq 0 ]; then
-        /opt/aws/bin/mon-put-data --metric-name "SwapAvailable" --value "$swap_avail" --unit "Kilobytes" $CLOUDWATCH_OPTS
+        /opt/aws/bin/mon-put-data --metric-name "SwapAvailable" --value "$swap_avail" --unit "$MEM_UNITS" $CLOUDWATCH_OPTS
     fi
 fi
 
 # Disk
 disk_total=`echo "$df_output" | tail -1 | tr -s ' ' | cut -d ' ' -f 2`
+disk_total=`expr $disk_total \* $KILO`
 disk_used=`echo "$df_output" | tail -1 | tr -s ' ' | cut -d ' ' -f 3`
+disk_used=`expr $disk_used \* $KILO`
 disk_avail=`echo "$df_output" | tail -1 | tr -s ' ' | cut -d ' ' -f 4`
+disk_avail=`expr $disk_avail \* $KILO`
 
 if [ $DEBUG -eq 1 ]; then
     echo "DiskTotal:$disk_total"
@@ -505,20 +568,22 @@ if [ $DISK_SPACE_UTIL -eq 1 -a -n "$DISK_PATH" -a $disk_total -gt 0 ]; then
 fi
 
 if [ $DISK_SPACE_USED -eq 1 -a -n "$DISK_PATH" ]; then
+    disk_used=`expr $disk_used / $DISK_SPACE_UNIT_DIV`
     if [ $VERBOSE -eq 1 ]; then
         echo "disk_used:$disk_used"
     fi
     if [ $VERIFY -eq 0 ]; then
-        /opt/aws/bin/mon-put-data --metric-name "DiskSpaceUsed" --value "$disk_used" --unit "Bytes" $CLOUDWATCH_OPTS
+        /opt/aws/bin/mon-put-data --metric-name "DiskSpaceUsed" --value "$disk_used" --unit "$DISK_SPACE_UNITS" $CLOUDWATCH_OPTS
     fi
 fi
 
 if [ $DISK_SPACE_AVAIL -eq 1 -a -n "$DISK_PATH" ]; then
+    disk_avail=`expr $disk_avail / $DISK_SPACE_UNIT_DIV`
     if [ $VERBOSE -eq 1 ]; then
         echo "disk_avail:$disk_avail"
     fi
     if [ $VERIFY -eq 0 ]; then
-        /opt/aws/bin/mon-put-data --metric-name "DiskSpaceAvailable" --value "$disk_avail" --unit "Bytes" $CLOUDWATCH_OPTS
+        /opt/aws/bin/mon-put-data --metric-name "DiskSpaceAvailable" --value "$disk_avail" --unit "$DISK_SPACE_UNITS" $CLOUDWATCH_OPTS
     fi
 fi
 
